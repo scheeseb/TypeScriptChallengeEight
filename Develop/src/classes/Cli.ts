b10db6afefb852594afe82bec626a41ac656356f
scheeseb <samuel.cheeseboro@gmail.com>
2025-01-02 14:38:34 -0600

    Added the wheelie method only if it is a Motorbike

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -391,7 +391,9 @@ class Cli {
               this.vehicles[i].reverse();
             }
           }
-        } else if (answers.action === "Tow") {
+        }
+        // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+        else if (answers.action === "Tow") {
           for (let i = 0; i < this.vehicles.length; i++) {
             const vehicle = this.vehicles[i];
             if (
@@ -402,9 +404,18 @@ class Cli {
             }
           }
         }
-        // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
-        else if (answers.action === "Select or create another vehicle") {
+        else if (answers.action === "Wheelie") {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            const vehicle = this.vehicles[i];
+            if (
+              vehicle.vin === this.selectedVehicleVin &&
+              vehicle instanceof Motorbike
+            ) {
+              vehicle.wheelie();
+            }
+          }
+        } else if (answers.action === "Select or create another vehicle") {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
           return;
